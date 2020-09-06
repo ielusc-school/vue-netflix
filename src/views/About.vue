17727b8c7b9ca5ce9ac1f3e80edffda7d1fd2550
Cristofer Sousa <cristofer.sousa@gmail.com>
2020-09-06 20:13:40 -0300

    add initials files

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,124 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <form>
+    <label for="">Nome</label> <br>
+    <input
+      type="text"
+      placeholder="Insira  nome"
+      v-model="name"> <br><br><br>
+
+    <label for="">E-mail</label> <br>
+    <input
+      type="email"
+     placeholder="Insira seu e-mail"
+      v-model="email"> <br><br><br>
+
+    <label for="">Insira seu CEP</label> <br>
+    <input
+      type="text"
+      placeholder="Insira seu Cep"
+      v-model="cep"> <br><br><br>
+
+     <textarea column="3"
+      v-model="message"
+      placeholder="Insira sua mensagem marota....">
+     </textarea>
+
+     <button
+      type="submit"
+      @click.prevent="sendData()">
+      Enviar
+      </button>
+     </form>
+
+    <div>
+      <ul>
+        <li>
+            Name: {{dados.name}} | E-mail {{dados.email}}
+            Endereço: {{dados.logradouro }} - {{dados.localidade}}
+            Bairro: {{dados.bairro}}
+            Cidade: {{dados.localidade}} - {{dados.uf}}
+            CEP: {{dados.cep}}
+        </li>
+      </ul>
+    </div>
+  <hr>
+  <input type="text" v-model="james"> <br>
+  <input type="text" :value="samsung"> <br>
+  <br> <br>
+  {{ james }} <br><br>
+  {{ samsung }}
+
+  <div>
+    <div v-for="filme in movies" :key="filme.id">
+      <img :src="filme.Poster" alt="">
+      <h1> {{filme.Title}} </h1>
+      <h3> {{filme.Year}} </h3>
+    </div>
+  </div>
+
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      name: '',
+      email: '',
+      cep: '',
+      message: '',
+      endereco: {},
+      dados: {},
+      samsung: 'apple',
+      james: 'jack sparrow',
+      movies: [],
+    };
+  },
+  watch: {
+    cep(value) { // consultando a minha propriedade data
+      if (value.length === 8) {
+        this.buscarCep(value);
+      }
+    },
+  },
+  created() {
+    this.getMovies();
+  },
+  methods: {
+    buscarCep(value) {
+      const url = `http://viacep.com.br/ws/${value}/json/`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          this.endereco = response;
+        });
+    },
+    getMovies() {
+      const url = 'http://www.omdbapi.com/?s=Harry&page=10&apikey=94e07ba9';
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          this.movies = response.Search;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    sendData() {
+      const data = {
+        name: this.name,
+        email: this.email,
+        cep: this.endereco,
+        message: this.message,
+      };
+      this.dados = data;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{name}} {{lastName}}
+    <h1> {{ fullName }} </h1>
+
+    <h1> Carros </h1>
+    <ul>
+      <li
+        v-for="car in cars"
+        :key="car.id">
+        {{car}}
+      </li>
+    </ul>
+
+      <h1> Carros2 </h1>
+
+    <ul class="cars2">
+      <li v-for="car2 in filterBlackCar" :key="car2.id">
+        {{car2}}
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      name: 'james',
+      lastName: 'bond',
+      cars: [
+        { id: 1, model: 'fusca', cor: 'black' },
+        { id: 2, model: 'fusca', cor: 'white' },
+        { id: 3, model: 'fusca', cor: 'blue' },
+        { id: 4, model: 'fusca', cor: 'red' },
+        { id: 5, model: 'gol', cor: 'blue' },
+        { id: 6, model: 'gol', cor: 'white' },
+        { id: 7, model: 'gol', cor: 'gray' },
+        { id: 8, model: 'gol', cor: 'red' },
+      ],
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.name} ${this.lastName}`;
+    },
+    filterBlackCar() {
+      const filterCar = this.cars.filter(({ cor }) => cor === 'red');
+      return filterCar;
+    },
   },
 };
 </script>
+
+<style>
+  .cars2 {
+    background: green;
+    color: black;
+  }
+</style>
